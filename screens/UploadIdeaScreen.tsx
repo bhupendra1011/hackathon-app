bd1a1501d59841731d538d95fba9bc6346013828
Bhupendra <bhupendra1011@gmail.com>
2021-06-30 13:42:21 +0530

    Adding expo-image-picker

--- screens/UploadIdeaScreen.tsx
+++ screens/UploadIdeaScreen.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import TextBox from '../components/TextBox';
 import Button from '../components/Button';
+
+import * as ImagePicker from 'expo-image-picker';
 
 
 import { Text, View } from '../components/Themed';
@@ -11,9 +13,38 @@ export default function UploadIdeaScreen() {
   const [title, setTitle] = React.useState<string>('');
   const [description, setDescription] = React.useState<string>('');
 
+  const [video, setVideo] = React.useState<string | null>(null);
+  const [duration, setDuration] = React.useState(0);
+
+
+  // reuesting camera permissions on mount , web its allowed by default
+  React.useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
   const uploadProject = () => { console.warn("You submitted !") }
 
-  const pickVideo = () => { console.warn("Pick Video from Library !") }
+  const pickVideo = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setVideo(result.uri);
+      setDuration(Math.floor(result.duration / 100)); // in seconds
+    }
+  }
 
 
   return (
