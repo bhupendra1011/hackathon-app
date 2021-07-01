94949d71263cc4abb545c4d328025f86ebc2b89c
Bhupendra <bhupendra1011@gmail.com>
2021-07-01 16:15:57 +0530

    added send feedback

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Pressable } from 'react-native'
 import Comment from './Comment'
 import { BottomSheetFlatList } from '@gorhom/bottom-sheet'
+import { Feather } from '@expo/vector-icons';
 
 
-import { Text, View } from './Themed';
+import { Text, useThemeColor, View } from './Themed';
+import TextBox from './TextBox';
 
 const Comments = ({ comments }) => {
+    const [newComment, setNewComment] = React.useState("");
+
+    const color = useThemeColor({}, 'text');
+
+    const submitCommit = () => {
+        console.warn("Submit Feedback");
+    }
+
+
+
     return (
         <View style={styles.container}>
+            <View style={styles.submitFeedback}>
+                <TextBox style={styles.textBox} placeholder="Your feedback on the project ..." value={newComment} onChangeText={setNewComment} />
+                <Pressable onPress={submitCommit}>
+                    <Feather name="send" size={24} color={color} />
+                </Pressable>
+            </View>
             <BottomSheetFlatList
                 data={comments}
                 renderItem={({ item }) => <Comment comment={item} />}
@@ -22,6 +40,16 @@ export default Comments
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 15
+        paddingHorizontal: 10
+    },
+    submitFeedback: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+
+    },
+    textBox: {
+        marginVertical: 10,
+        marginRight: 15
     }
 })
